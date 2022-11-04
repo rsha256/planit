92853e37b0c9d819275981c1b4d724be61b578da
planit <planit@tanjeffreyz.github.io>
2022-11-04 04:25:56 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -55,6 +55,13 @@ assignments = {
       "dueDate": "2022-11-03T21:00:00",
       "link": "https://www.gradescope.com/courses/420925",
       "submitted": false
+    },
+    {
+      "title": "hw7",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2022-11-10T21:00:00",
+      "link": "https://www.gradescope.com/courses/420925",
+      "submitted": false
     }
   ],
   "CS 182/282A": [
