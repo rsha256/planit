b1b81a25a70cbe3ca6ca6bd198e7d1ade4d8de20
planit <planit@tanjeffreyz.github.io>
2022-11-08 22:44:25 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -935,7 +935,7 @@ assignments = {
     {
       "title": "Homework 8 Self-Grades",
       "course": "EE 120",
-      "dueDate": "2022-11-18T23:59:00",
+      "dueDate": "2022-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/401330",
       "submitted": false
     },
