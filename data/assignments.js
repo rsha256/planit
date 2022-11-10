f31926215da292e84d05b87716f85dccedd8e279
planit <planit@tanjeffreyz.github.io>
2022-11-10 07:41:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1106,8 +1106,8 @@ assignments = {
       "title": "HW9 (pdf)",
       "course": "EECS 106A",
       "dueDate": "2022-11-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/429119",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/429119/assignments/2416111/submissions/147782845",
+      "submitted": true
     }
   ]
 };
