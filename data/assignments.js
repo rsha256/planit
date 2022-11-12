377996190ff5a1f7ec0267d48cba619cdc0d5010
planit <planit@tanjeffreyz.github.io>
2022-11-12 20:43:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -386,6 +386,13 @@ assignments = {
       "dueDate": "2022-11-14T23:00:00",
       "link": "https://www.gradescope.com/courses/425603",
       "submitted": false
+    },
+    {
+      "title": "HW 8 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-11-14T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
     }
   ],
   "CS 194-26 / 294-26": [
