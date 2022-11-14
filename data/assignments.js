550375df1067ec09584d0779b78380ee42d99518
planit <planit@tanjeffreyz.github.io>
2022-11-14 08:50:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -994,6 +994,13 @@ assignments = {
       "dueDate": "2022-11-21T23:59:00",
       "link": "https://www.gradescope.com/courses/401330",
       "submitted": false
+    },
+    {
+      "title": "Homework 9 Self Grades",
+      "course": "EE 120",
+      "dueDate": "2022-11-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/401330",
+      "submitted": false
     }
   ],
   "EECS 106A": [
