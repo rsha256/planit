e8461356e5ba5c4b49b035c8b8a19d89cd5075db
planit <planit@tanjeffreyz.github.io>
2022-12-01 09:40:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1050,6 +1050,13 @@ assignments = {
       "dueDate": "2022-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/401330/assignments/2436681/submissions/148654758",
       "submitted": true
+    },
+    {
+      "title": "Orientation to the Gradescope Exam Format",
+      "course": "EE 120",
+      "dueDate": "2022-12-01T12:15:00",
+      "link": "https://www.gradescope.com/courses/401330/assignments/2472619/submissions/new",
+      "submitted": false
     }
   ],
   "EECS 106A": [
