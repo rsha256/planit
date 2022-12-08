533b8cd237709a8d50d735ca1fcef2cb16ed61df
planit <planit@tanjeffreyz.github.io>
2022-12-08 21:38:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1087,13 +1087,6 @@ assignments = {
       "submitted": true
     },
     {
-      "title": "EE120-F22-MT2-2022-12-01-Pledge of Academic Integrity",
-      "course": "EE 120",
-      "dueDate": "2022-12-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/401330/assignments/2474587/submissions/151810092",
-      "submitted": true
-    },
-    {
       "title": "EE120-F22-MT2-2022-12-01-EXAM-QUESTIONS",
       "course": "EE 120",
       "dueDate": "2022-12-05T00:59:00",
