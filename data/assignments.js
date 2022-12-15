e005a9c59cbb564bf85ce2d885512436d8d663c5
planit <planit@tanjeffreyz.github.io>
2022-12-15 08:42:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -920,8 +920,8 @@ assignments = {
       "title": "Final Project",
       "course": "CS 285",
       "dueDate": "2022-12-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/417906",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/417906/assignments/2466221/submissions/154501711",
+      "submitted": true
     }
   ],
   "EE 120": [
