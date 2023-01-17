8097a6d399bf988fe8a3fbaa6772e481fb72edea
planit <planit@tanjeffreyz.github.io>
2023-01-17 08:41:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,12 @@
 assignments = {
+  "COMPSCI C191-LEC-001": [
+    {
+      "title": "Homework 1",
+      "course": "COMPSCI C191-LEC-001",
+      "dueDate": "2023-01-24T00:00:00",
+      "link": "https://www.gradescope.com/courses/493981",
+      "submitted": false
+    }
+  ],
   "CS 184/284A": []
 };
