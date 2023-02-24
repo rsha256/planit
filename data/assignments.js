1ffa7770c8a5cab22902344688c7372c4bc6b974
planit <planit@tanjeffreyz.github.io>
2023-02-24 20:39:43 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -150,6 +150,13 @@ assignments = {
       "dueDate": "2023-02-21T23:59:00",
       "link": "https://www.gradescope.com/courses/499404/assignments/2679215/submissions/162839989",
       "submitted": true
+    },
+    {
+      "title": "Discussion 6",
+      "course": "Stat157/260",
+      "dueDate": "2023-02-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/499404",
+      "submitted": false
     }
   ]
 };
