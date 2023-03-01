8ccc140beee709daf8b1048aa5d44c14746e29d7
planit <planit@tanjeffreyz.github.io>
2023-03-01 16:50:25 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -17,14 +17,14 @@ assignments = {
     {
       "title": "Project 2 Checkpoint",
       "course": "CS 184/284A",
-      "dueDate": "2023-02-21T23:59:00",
+      "dueDate": "2023-02-23T23:59:00",
       "link": "https://www.gradescope.com/courses/483806/assignments/2658241/submissions/162089511",
       "submitted": true
     },
     {
       "title": "Project 2: MeshEdit",
       "course": "CS 184/284A",
-      "dueDate": "2023-02-28T23:59:00",
+      "dueDate": "2023-03-02T23:59:00",
       "link": "https://www.gradescope.com/courses/483806/assignments/2659849/submissions/165483038",
       "submitted": true
     },
