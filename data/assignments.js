c0cec0091107c30f432e8a5f8b5e85626b36c451
planit <planit@tanjeffreyz.github.io>
2023-03-02 07:38:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -39,8 +39,8 @@ assignments = {
       "title": "Project 3-1 Checkpoint",
       "course": "CS 184/284A",
       "dueDate": "2023-03-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806/assignments/2688745/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/483806/assignments/2688745/submissions/165807521",
+      "submitted": true
     },
     {
       "title": "Project 3-2 Checkpoint",
@@ -176,8 +176,8 @@ assignments = {
       "title": "HW 6",
       "course": "Stat157/260",
       "dueDate": "2023-03-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/499404",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/499404/assignments/2703354/submissions/165805586",
+      "submitted": true
     },
     {
       "title": "HW 7",
