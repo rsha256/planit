41f896b37155db8598adb73aed8daf26d10bec7d
planit <planit@tanjeffreyz.github.io>
2023-03-03 08:42:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -25,7 +25,7 @@ assignments = {
       "title": "Project 2: MeshEdit",
       "course": "CS 184/284A",
       "dueDate": "2023-03-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806/assignments/2659849/submissions/165483038",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2659849/submissions/166154488",
       "submitted": true
     },
     {
