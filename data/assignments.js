165d8f7b4af30cb4dcaa39a665ba58f2346e3000
planit <planit@tanjeffreyz.github.io>
2023-03-22 07:38:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -260,8 +260,8 @@ assignments = {
       "title": "HW 9",
       "course": "Stat157/260",
       "dueDate": "2023-03-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/499404",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/499404/assignments/2745026/submissions/169964013",
+      "submitted": true
     },
     {
       "title": "Discussion 9",
