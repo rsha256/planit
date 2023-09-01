aaeb27225a969d29b91bc3d3432b7cf25362b7eb
planit <planit@tanjeffreyz.github.io>
2023-09-01 06:38:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -38,7 +38,7 @@ assignments = {
     {
       "title": "Homework 1 for Stat 156",
       "course": "Stat 156/256",
-      "dueDate": "2023-09-05T17:00:00",
+      "dueDate": "2023-09-05T23:59:00",
       "link": "https://www.gradescope.com/courses/572925",
       "submitted": false
     },
@@ -46,6 +46,20 @@ assignments = {
       "title": "Reading Assignment 1 for Stat 256",
       "course": "Stat 156/256",
       "dueDate": "2023-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/572925",
+      "submitted": false
+    },
+    {
+      "title": "Reading Assignment 2 for Stat 256",
+      "course": "Stat 156/256",
+      "dueDate": "2023-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/572925",
+      "submitted": false
+    },
+    {
+      "title": "Homework 2 for Stat 156",
+      "course": "Stat 156/256",
+      "dueDate": "2023-09-15T23:59:00",
       "link": "https://www.gradescope.com/courses/572925",
       "submitted": false
     }
