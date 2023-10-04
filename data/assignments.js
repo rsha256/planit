8dc11ddf7a0172094c7ff7afa764d9c9b6469d5f
planit <planit@tanjeffreyz.github.io>
2023-10-04 18:39:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -231,6 +231,13 @@ assignments = {
       "dueDate": "2023-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/585729",
       "submitted": false
+    },
+    {
+      "title": "Homework 5 post-mortem",
+      "course": "STAT 210A",
+      "dueDate": "2023-09-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/585729",
+      "submitted": false
     }
   ],
   "Stat 156/256": [
