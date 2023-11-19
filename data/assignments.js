cd60f593941b99b6beaf28e96996ed92a7e9ff25
planit <planit@tanjeffreyz.github.io>
2023-11-19 02:43:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -348,6 +348,13 @@ assignments = {
       "dueDate": "2023-11-15T23:59:00",
       "link": "https://www.gradescope.com/courses/572432/assignments/3666358/submissions/213520848",
       "submitted": true
+    },
+    {
+      "title": "HW12",
+      "course": "Math 221",
+      "dueDate": "2023-11-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/572432",
+      "submitted": false
     }
   ],
   "STAT 210A": [
