41452f36628a99d6dd293e4af0c692fff78a70cb
planit <planit@tanjeffreyz.github.io>
2023-11-28 17:38:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -373,7 +373,7 @@ assignments = {
     {
       "title": "HW13",
       "course": "Math 221",
-      "dueDate": "2023-11-29T23:59:00",
+      "dueDate": "2023-12-01T23:59:00",
       "link": "https://www.gradescope.com/courses/572432",
       "submitted": false
     }
