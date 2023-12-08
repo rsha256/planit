df005902493b2fa3dc588bbca3b3f35709f9532f
planit <planit@tanjeffreyz.github.io>
2023-12-08 22:38:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -187,7 +187,7 @@ assignments = {
     {
       "title": "Lab Redo",
       "course": "DMA23",
-      "dueDate": "2023-12-09T12:00:00",
+      "dueDate": "2023-12-10T23:59:00",
       "link": "https://www.gradescope.com/courses/589310/assignments/3709694/submissions/216060575",
       "submitted": true
     }
