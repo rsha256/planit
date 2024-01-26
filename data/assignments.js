700d9d7fb4e53d90cfd0bb8b6b17e4dd981de7ac
planit <planit@tanjeffreyz.github.io>
2024-01-26 19:37:57 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -27,6 +27,13 @@ assignments = {
       "dueDate": "2024-02-02T17:00:00",
       "link": "https://www.gradescope.com/courses/711377",
       "submitted": false
+    },
+    {
+      "title": "Vitamin 2",
+      "course": "Data 102",
+      "dueDate": "2024-01-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/711377/assignments/4008832/submissions/new",
+      "submitted": false
     }
   ],
   "MATH 113-LEC-005": [
