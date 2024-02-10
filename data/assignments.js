f14d96b7e285dc7d44d36b447eccd0ef8989de98
planit <planit@tanjeffreyz.github.io>
2024-02-10 11:38:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -185,8 +185,8 @@ assignments = {
       "title": "Practice Quiz 5: Asymptotic Equipartition and Typical Sets",
       "course": "STAT 157-SEM-001",
       "dueDate": "2024-02-13T15:30:00",
-      "link": "https://www.gradescope.com/courses/708637/assignments/4081127/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/708637/assignments/4081127/submissions/229503053",
+      "submitted": true
     }
   ]
 };
