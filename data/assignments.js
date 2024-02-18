72d3ac0befa27b97b2610687f6711b8ba2f488de
planit <planit@tanjeffreyz.github.io>
2024-02-18 01:35:43 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -127,6 +127,13 @@ assignments = {
       "dueDate": "2024-02-18T23:59:00",
       "link": "https://www.gradescope.com/courses/708525",
       "submitted": false
+    },
+    {
+      "title": "HW 4",
+      "course": "MATH 113-LEC-005",
+      "dueDate": "2024-02-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/708525",
+      "submitted": false
     }
   ],
   "MATH 228B-LEC-001": [
