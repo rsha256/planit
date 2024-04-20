4d475b5f4fd493a6e66b3b34b731078bb5b89e67
planit <planit@tanjeffreyz.github.io>
2024-04-20 18:38:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -272,6 +272,13 @@ assignments = {
       "dueDate": "2024-04-21T23:59:00",
       "link": "https://www.gradescope.com/courses/711377",
       "submitted": false
+    },
+    {
+      "title": "Vitamin 12",
+      "course": "Data 102",
+      "dueDate": "2024-04-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/711377/assignments/4387338/submissions/new",
+      "submitted": false
     }
   ],
   "Data 102 Vitamins": [
