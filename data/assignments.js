1f86caeec729a62aaa88fe2baa48b450181dfa3d
planit <planit@tanjeffreyz.github.io>
2024-05-06 19:38:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -340,8 +340,8 @@ assignments = {
       "title": "Final Project Code",
       "course": "Data 102",
       "dueDate": "2024-05-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/711377",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/711377/assignments/4457734/submissions/252612107",
+      "submitted": true
     },
     {
       "title": "Final Project Group Member Evalution",
