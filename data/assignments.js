7fcb6fab162b6e4fec57219d6f9d9307be03e5c8
planit <planit@tanjeffreyz.github.io>
2025-01-28 06:44:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "CS 194/294-280": [],
+  "CS 194/294-280": [
+    {
+      "title": "Lecture 2 Reading Summary & Questions",
+      "course": "CS 194/294-280",
+      "dueDate": "2025-02-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/969624/assignments/5683502/submissions/new",
+      "submitted": false
+    }
+  ],
   "STAT 238": []
 };
