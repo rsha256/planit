f06755d5a10ce87eb7cd17849528bf73b8dec0fb
planit <planit@tanjeffreyz.github.io>
2025-02-07 17:38:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -25,8 +25,8 @@ assignments = {
       "title": "Lecture 3 Reading Summary",
       "course": "CS 194/294-280",
       "dueDate": "2025-02-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/969624/assignments/5733085/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/969624/assignments/5733085/submissions/306153191",
+      "submitted": true
     },
     {
       "title": "Project Proposal",
