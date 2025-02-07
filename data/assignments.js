1b080fc2670f75ed330e3ab935fec785b82ef8d4
planit <planit@tanjeffreyz.github.io>
2025-02-07 04:42:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -39,8 +39,8 @@ assignments = {
       "title": "Quiz 2",
       "course": "CS 194/294-280",
       "dueDate": "2025-02-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/969624/assignments/5737312/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/969624/assignments/5737312/submissions/306030441",
+      "submitted": true
     }
   ],
   "STAT 238": [
