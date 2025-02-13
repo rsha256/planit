1a31ad9587fb93bd1bba18eaa28fe9db38c9451a
planit <planit@tanjeffreyz.github.io>
2025-02-13 17:38:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -113,6 +113,13 @@ assignments = {
       "dueDate": "2025-02-12T11:00:00",
       "link": "https://www.gradescope.com/courses/961711",
       "submitted": false
+    },
+    {
+      "title": "HW 2",
+      "course": "STAT 238",
+      "dueDate": "2025-02-28T17:00:00",
+      "link": "https://www.gradescope.com/courses/961711",
+      "submitted": false
     }
   ],
   "STAT 260": [
