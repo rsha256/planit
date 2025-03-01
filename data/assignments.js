1da4574ca1a660c8c7e4b0e9ba79d3893be6efeb
planit <planit@tanjeffreyz.github.io>
2025-03-01 02:12:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -241,6 +241,13 @@ assignments = {
       "dueDate": "2025-05-10T23:59:00",
       "link": "https://www.gradescope.com/courses/970705",
       "submitted": false
+    },
+    {
+      "title": "HW1 Solution",
+      "course": "STAT 260",
+      "dueDate": "2025-05-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/970705",
+      "submitted": false
     }
   ]
 };
