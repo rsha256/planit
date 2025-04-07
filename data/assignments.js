7a0b3ac541a21cede2a3cc26d2a7346c3b4adb0c
planit <planit@tanjeffreyz.github.io>
2025-04-07 23:40:36 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -414,8 +414,8 @@ assignments = {
       "title": "[Info] Class Project Instruction",
       "course": "STAT 260",
       "dueDate": "2025-05-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/970705",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/970705/assignments/5853540/submissions/323269674",
+      "submitted": true
     },
     {
       "title": "[Info] HW1 Solution",
