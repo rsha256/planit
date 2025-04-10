d03d2bf41170973e5880114b563aaed16b30146a
planit <planit@tanjeffreyz.github.io>
2025-04-10 05:42:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -160,6 +160,13 @@ assignments = {
       "dueDate": "2025-04-13T23:59:00",
       "link": "https://www.gradescope.com/courses/969624/assignments/6060483/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Lab 1, Extra Credit - Verifiable Code Generation Dataset",
+      "course": "CS 194/294-280",
+      "dueDate": "2025-04-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/969624/assignments/6066861/submissions/new",
+      "submitted": false
     }
   ],
   "STAT 238": [
