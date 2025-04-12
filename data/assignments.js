4acb5e3c7080d0a2be749605169679b0cf19bcd0
planit <planit@tanjeffreyz.github.io>
2025-04-12 21:38:57 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -74,7 +74,7 @@ assignments = {
       "title": "Attendance Tracker",
       "course": "CS 194/294-280",
       "dueDate": "2025-05-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/969624/assignments/5864831/submissions/321786578",
+      "link": "https://www.gradescope.com/courses/969624/assignments/5864831/submissions/324840808",
       "submitted": true
     },
     {
